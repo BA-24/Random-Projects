e51e7131623e779fb2ca9593efafd2e9d43ed5fa
BA-24 <65275759+BA-24@users.noreply.github.com>
2020-06-07 12:33:07 +0200

    Update hotkeys 2.0.py

--- hotkeys/hotkeys 2.0.py
+++ hotkeys/hotkeys 2.0.py
@@ -1,52 +1,81 @@
-print_keys = False
-
-
 print('starting application...')
 
 from pynput.keyboard import Key, Listener
 import keyboard
 import time
+import os
+from os import path
 check = True
 
-#gets detection keys
-f = open('hotkeys.txt', 'r')
-keys = []
-for r in f:    
-    if r != '\n':
-        keys = keys + [r.split()[0]]
-f.close()
+#checks for profile folder
+if path.exists('./profiles'):
+    print('profile folder found. select profile:')
+    profiles = os.listdir('./profiles')
+    for x in range(len(profiles)):
+        print(str(x+1) + '. ' + profiles[x])
+    loop = True
+    while loop:
+        try:
+            profile = profiles[(int(input())) - 1]
+            loop = False
+        except ValueError:
+            print('Please enter a number')
 
-#gets messages
-f = open('hotkeys.txt', 'r')
-msg = []
-for r in f:    
-    if r != '\n':
-        msg = msg + [r.replace( ((r.split()[0]) + ' '), '', 1).replace('\n', '', 1)]
-f.close()
+    file = './profiles/' + profile
 
-def write(message, latency=0.1):
-    keyboard.press_and_release('enter')
-    time.sleep(latency)
-    keyboard.write(message)
-    keyboard.press_and_release('enter')
+else:
+    try:
+        f = open('hotkeys.txt')
+        f.close()
+        print('hotkeys.txt found.')
+        file = 'hotkeys.txt'
+    except:
+        print('no hotkey files found.')
+        file = None
+        
+    
+        
 
-def on_press(key):
-    global check
-    if check == True:
-        a = 0
-        for x in keys:
-            if x == str(key):
-                write(msg[a])
-            a = a + 1
+if file != None:
 
-    if print_keys == True:
-        print(key)
+    #gets detection keys
+    f = open(file, 'r')
+    keys = []
+    for r in f:    
+        if r != '\n':
+            keys = keys + [r.split()[0]]
+    f.close()
 
-def on_release(key):
-    global check
-    check = True
+    #gets messages
+    f = open(file, 'r')
+    msg = []
+    for r in f:    
+        if r != '\n':
+            msg = msg + [r.replace( ((r.split()[0]) + ' '), '', 1).replace('\n', '', 1)]
+    f.close()
 
-print('application started')
-with Listener(on_press=on_press, on_release=on_release) as listener: listener.join()
+    def write(message, latency=0.1):
+        keyboard.press_and_release('enter')
+        time.sleep(latency)
+        keyboard.write(message)
+        keyboard.press_and_release('enter')
 
-print('application started')
+    def on_press(key):
+        global check
+        if check == True:
+            a = 0
+            for x in keys:
+                if x == str(key):
+                    write(msg[a])
+                a = a + 1
+
+    def on_release(key):
+        global check
+        check = True
+
+    print('application started')
+    with Listener(on_press=on_press, on_release=on_release) as listener: listener.join()
+
+elif file == None:
+    while True:
+        pass
